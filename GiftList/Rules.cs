61f77b48a135775d0db5d6dd0d1ef60ccc1946b3
Joe Gotthelf <jgotthelf@gmail.com>
2014-07-17 18:21:18 -0400

    Initial Commit

---
+++ GiftList/Rules.cs
@@ -0,0 +1,11 @@
+﻿namespace GiftList {
+	public partial class xmaslistlegacyEntities {
+		partial void OnContextCreated() {
+			//this.SavingChanges += new System.EventHandler(xmaslistlegacyEntities_SavingChanges);
+		}
+
+		void xmaslistlegacyEntities_SavingChanges(object sender, System.EventArgs e) {
+			throw new System.NotImplementedException();
+		}
+	}
+}
